3fc838dfdd0a055a29a627c1c1c934ec553a3c23
Miranda Quinene <mequinene@student.fullsail.edu>
2025-12-07 18:13:33 -0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+//   entry: './src/index.js',
+entry: './src/ts/index.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
