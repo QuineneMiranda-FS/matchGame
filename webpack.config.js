862830ad45e9ae73a6908199d1805384ef0861e8
Miranda Quinene <mequinene@student.fullsail.edu>
2025-12-07 19:20:30 -0500

    ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,29 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-module.exports = {
-  entry: "./src/index.ts",
-  output: {
-    filename: "bundle.js",
-
-    path: path.resolve(__dirname, "build"),
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    module.exports = {
+      entry: './src/index.ts', // Your entry point
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/, // Apply ts-loader to .ts and .tsx files
+            use: 'ts-loader',
+            exclude: /node_modules/, // Exclude node_modules
+          },
+        ],
+      },
+      resolve: {
+        extensions: ['.tsx', '.ts', '.js'], // Resolve these extensions
+      },
+      output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     },
     extensions: [".js", ".scss", ".css"],
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Test for .js files
+        test: /\.js|\.jsx$/ // Test for .js files
         exclude: /node_modules/, // Exclude node_modules
         use: {
           loader: "babel-loader", // Use Babel to transpile ES6 to ES5
